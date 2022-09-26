03ce47f9d0b4d1c7b2c7b9d1e538eff8c7157cf7
frol_cao <nkita_0404@mail.ru>
2022-09-26 18:22:04 +0300

    Две задачи c#

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int getSecondDigit(int number)
+{
+  int secondNumber = new int();
+  secondNumber = number % 100 / 10;
+  return secondNumber;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = getSecondDigit(number);
+Console.WriteLine($"Второе число от {number} - {secondDigit}");
---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+void weekend(int dayOfTheWeek)
+{
+    if (dayOfTheWeek == 6 || dayOfTheWeek == 7) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");  
+}
+
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+weekend(dayOfTheWeek);
